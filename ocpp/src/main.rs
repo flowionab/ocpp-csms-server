f6a23ea8e918331bda452905a5a276b5570b38ed
Joatin Granlund <granlundjoatin@gmail.com>
2025-01-07 21:42:00 +0100

    fixes

--- ocpp/src/main.rs
+++ ocpp/src/main.rs
@@ -9,9 +9,10 @@ use crate::ocpp::start_ocpp_server;
 use crate::server::start_server;
 use shared::{configure_tracing, read_config, SqlxDataStore};
 use sqlx::postgres::PgPoolOptions;
+use std::env;
 use std::sync::Arc;
 use tokio::try_join;
-use tracing::info;
+use tracing::{error, info, warn};
 
 pub mod ocpp_csms_server {
     tonic::include_proto!("ocpp_csms_server");
@@ -25,10 +26,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     config.print_config_warnings();
 
     info!("connecting to database");
+    let database_url = env::var("DATABASE_URL").unwrap_or_else(|_| {
+        warn!("No DATABASE_URL env var provided, attempting to connect to database at localhost");
+        "postgres://postgres:password@localhost/postgres".to_string()
+    });
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:password@localhost/postgres")
-        .await?;
+        .connect(&database_url)
+        .await
+        .inspect_err(|e| {
+            error!(
+                error_message = e.to_string(),
+                "Could not connect to database"
+            );
+        })?;
 
     let data_store = Arc::new(SqlxDataStore::setup(pool).await?);
 
