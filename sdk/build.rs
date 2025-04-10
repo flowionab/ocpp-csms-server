5bb582c77a417b09ecd6fbd20df9c8240a54ed16
Joatin Granlund <granlundjoatin@gmail.com>
2025-04-10 11:11:24 +0200

    add sdk

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .compile_protos(&["../proto/api_service.proto"], &["../proto"])
+        .compile_protos(&["proto/api_service.proto"], &["proto"])
         .unwrap();
     Ok(())
 }
